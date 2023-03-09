46199ff62475a6c1f4efeb6e7f754a582f238dd7
bsmith86dev <bsmith86.dev@gmail.com>
2023-03-09 09:00:34 -0600

    f

--- src/components/scRow/index.js
+++ src/components/scRow/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import movie from '../../images/movie.jpg'
-import pcss from '../../images/pcss.png'
+import pc from '../../images/pc.png'
 import Screenshot from '../screenshots'
 import './index.css'
 
@@ -8,7 +8,7 @@ const ScreenshotsRow = () => {
     return (
         <div className="screenshots-row">
           <a href='https://moviehunt.onrender.com'><Screenshot imageUrl={movie} alt="Screenshot 1" description="Movie Hunt: Using the IMDB api, this all allows users to sech the database and receive all pertinent information for the searched movie and add it to a favorites list for later.  " /></a>
-          <a href='https://pc-super-store.onrender.com/'><Screenshot imageUrl={pcss} alt="Screenshot 2" description="PC Super-Store: This app is a basic E-commerce site with computer parts.  Utilizing passport to create and verify users and keep track of users orders. " /></a>
+          <a href='https://pc-super-store.onrender.com/'><Screenshot imageUrl={pc} alt="Screenshot 2" description="PC Super-Store: This app is a basic E-commerce site with computer parts.  Utilizing passport to create and verify users and keep track of users orders. " /></a>
           
         </div>
       );
